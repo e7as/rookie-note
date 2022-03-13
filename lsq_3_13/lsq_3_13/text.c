904ebf97e38d7adc14e63f41732c404bd0cfd692
e7as <2424594788@qq.com>
2022-03-14 00:14:27 +0800

    3.13数据的存储

--- lsq_3_13/lsq_3_13/text.c
+++ lsq_3_13/lsq_3_13/text.c
@@ -1,8 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#include<string.h>
+
+void my_strcpy(char* arr1, char* arr2)
+{
+	if (strcmp(arr2,'\0')!=0)
+	{
+		*arr1 = arr2;
+		my_strcpy(++arr1, ++arr2);
+	}
+	else
+	{
+		*arr1 = *arr2;
+	}
+}
 
 int main()
 {
-
+	char arr1[] = "**********";
+	char arr2[] = {'b', 'i', 't','\0'};
+	my_strcpy(arr1, arr2);
+	printf("%s\n", arr1);
 	return 0;
 }
