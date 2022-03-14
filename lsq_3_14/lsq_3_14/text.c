f1e60109ffa2e95a6d8ac2d7c7c1388150eb6d2b
e7as <2424594788@qq.com>
2022-03-15 00:06:21 +0800

    3_14

---
+++ lsq_3_14/lsq_3_14/text.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+
+}
+
+//int main()
+//{
+//	int i = 0;
+//	int arr1[] = { 1, 2, 3, 4, 5 };
+//	int arr2[] = { 2, 3, 4, 5, 6 };
+//	int arr3[] = { 3, 4, 5, 6, 7 };
+//	int* parr[] = { arr1, arr2, arr3 };
+//	int** pparr[] = { parr };
+//	printf("%d\n", **(pparr[0]));
+//	printf("%d\n", ***(pparr));
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[] = { 1 };
+//	int* parr[] = { arr };
+//	printf("%d\n", arr);
+//}
+
+//int main()
+//{
+//	int arr[] = { 1, 2, 3 };
+//	printf("%p ", *arr);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char arr[] = {'1','2',0,'3'};
+//	char* pc = arr;
+//	printf("%s\n",arr);
+//	printf("%s\n",pc);
+//	return 0;
+//}
