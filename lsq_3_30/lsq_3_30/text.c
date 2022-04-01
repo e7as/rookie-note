635effbb0904305ae80d916d0dde70861e20110a
e7as <2424594788@qq.com>
2022-04-01 23:46:43 +0800

    lsq_3_30

---
+++ lsq_3_30/lsq_3_30/text.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+#define PRINT(X) printf("the value of " #X " is %d\n",X)
+int main()
+{
+	int a = 10;
+	int b = 20;
+	PRINT(a);
+	PRINT(b);
+	return 0;
+}
