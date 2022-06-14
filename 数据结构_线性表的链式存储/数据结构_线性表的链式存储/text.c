9d3480b8fb66b508b1ac7064e3a89344c0e97a02
e7as <2424594788@qq.com>
2022-06-14 17:32:16 +0800

    数据结构_线性表的链式存储

--- 数据结构_线性表的链式存储/数据结构_线性表的链式存储/text.c
+++ 数据结构_线性表的链式存储/数据结构_线性表的链式存储/text.c
@@ -7,7 +7,6 @@ int main()
 	linklist p=NULL;
 	p=final_insert(p,3);
 	printf("%-5d%-5d%-5s\n", p->next->next->c.a, p->next->next->c.b, p->next->next->c.d);
-	clear_linklist(p);
 	cc=emp_linklist(p);
 	if (cc)
 		printf("%-5d%-5d%-5s\n", p->next->next->c.a, p->next->next->c.b, p->next->next->c.d);
